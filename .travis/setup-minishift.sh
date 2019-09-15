3bd73571a6667d91afb20652c025c7adc4c718ed
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-14 23:23:06 -0400

    add registry29

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -16,7 +16,7 @@ else
 
     echo "Bring up openshift cluster"
     mkdir -p "$HOME/.occluster"
-    ./oc cluster up  --base-dir="$HOME/.occluster" --skip-registry-check=true --image=registry.access.redhat.com/openshift3/ose-control-plane:v3.11
+    ./oc cluster up  --base-dir="$HOME/.occluster" --skip-registry-check=true --image=registry.access.redhat.com/openshift3/ose-control-plane:v3.10
     ./oc login -u system:admin
     echo "Creating new project $QUAY_NAMESPACE"
     ./oc new-project $QUAY_NAMESPACE
