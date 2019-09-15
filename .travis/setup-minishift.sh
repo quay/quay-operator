33453f78377f89d58c2d54cc7ef48de2671a860d
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-14 23:55:25 -0400

    add registry34

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -11,18 +11,20 @@ elif [[ -z "${RH_PASSWORD}" ]]; then
 elif [[ -z "${RH_USERNAME}" ]]; then
     echo "RH_USERNAME environment variable not set"
 else
-    wget https://mirror.openshift.com/pub/openshift-v3/clients/${OPENSHIFT_VERSION}/linux/oc.tar.gz
-    tar xvzf oc.tar.gz
+    #wget https://mirror.openshift.com/pub/openshift-v3/clients/${OPENSHIFT_VERSION}/linux/oc.tar.gz
+    #tar xvzf oc.tar.gz
+    echo "Logging into quay.io"
+    docker login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
+    cp ~/.docker/config.json ./
+    docker pull quay.io/openshift/origin-node:v3.11
+    sudo docker cp $(docker create docker.io/openshift/origin:v3.11):/bin/oc /usr/local/bin/oc
 
     echo "Bring up openshift cluster"
     mkdir -p "$HOME/.occluster"
     rm -r ~/.kube
-    ./oc cluster up  --base-dir="$HOME/.occluster" --skip-registry-check=true --image=registry.access.redhat.com/openshift3/ose-control-plane:v3.11.141-3
+    ./oc cluster up  --base-dir="$HOME/.occluster" --skip-registry-check=true --image=quay.io/openshift/origin-node:v3.11
     ./oc login -u system:admin
     echo "Creating new project $QUAY_NAMESPACE"
     ./oc new-project $QUAY_NAMESPACE
-    echo "Logging into quay.io"
-    docker login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
-    cp ~/.docker/config.json ./
     ./oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=config.json" --type='kubernetes.io/dockerconfigjson'
 fi
