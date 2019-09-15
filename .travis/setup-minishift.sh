1f3e01f093527011fbfcb6ed34f9959c99ddb561
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-14 21:19:38 -0400

    add registry8

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -15,7 +15,7 @@ else
     tar xvzf oc.tar.gz
 
     echo "Bring up openshift cluster"
-    ./oc cluster up --image=registry.access.redhat.com/openshift3/ose-control-plane:v3.11 --create-machine
+    ./oc cluster up --image=registry.access.redhat.com/openshift3/ose-control-plane:v3.11
     ./oc login -u system:admin
     echo "Creating new project $QUAY_NAMESPACE"
     ./oc new-project $QUAY_NAMESPACE
