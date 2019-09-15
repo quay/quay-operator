92a2dd9773a0c7ce42e45bbc2d772fadcdb20d62
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-15 10:42:23 -0400

    add registry51

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -1,7 +1,7 @@
 #REMOVE BEFORE MERGING
 export QUAY_PASSWORD="O81WSHRSJR14UAZBK54GQHJS0P1V4CLWAJV1X2C4SD7KO59CQ9N3RE12612XU1HR"
 export QUAY_USERNAME="redhat+quay"
-
+export KUBE_URL="https://storage.googleapis.com/kubernetes-release/release/v1.10.13/bin/linux/amd64/kubectl"
 if [[ -z "${QUAY_PASSWORD}" ]]; then
     echo "QUAY_PASSWORD environment variable not set"
 elif [[ -z "${QUAY_USERNAME}" ]]; then
@@ -11,7 +11,7 @@ elif [[ -z "${RH_PASSWORD}" ]]; then
 elif [[ -z "${RH_USERNAME}" ]]; then
     echo "RH_USERNAME environment variable not set"
 else
-    wget https://mirror.openshift.com/pub/openshift-v3/clients/${OPENSHIFT_VERSION}/linux/oc.tar.gz
+    sudo wget -qO- https://mirror.openshift.com/pub/openshift-v3/clients/${OPENSHIFT_VERSION}/linux/oc.tar.gz | sudo tar -xvz -C /bin
     tar xvzf oc.tar.gz
     echo "Logging into quay.io"
     docker login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
@@ -20,11 +20,10 @@ else
     #sudo docker cp $(docker create docker.io/openshift/origin:v3.11):/bin/oc /usr/local/bin/oc
 
     echo "Bring up openshift cluster"
-    mkdir -p "$HOME/.occluster"
-    rm -r ~/.kube
-    ./oc cluster up  --base-dir="$HOME/.occluster" --image=registry.access.redhat.com/openshift3/ose-control-plane:v3.10.45-7
-    ./oc login -u system:admin
+    IP_ADDR=$(ip addr show $DEV | awk '/inet /{ gsub("/.*", ""); print $2}')
+    oc cluster up --public-hostname=${IP_ADDR} --routing-suffix=${IP_ADDR}.nip.io --base-dir=/home/travis/ocp
+    oc login -u system:admin
     echo "Creating new project $QUAY_NAMESPACE"
-    ./oc new-project $QUAY_NAMESPACE
-    ./oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=config.json" --type='kubernetes.io/dockerconfigjson'
+    oc new-project $QUAY_NAMESPACE
+    oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=config.json" --type='kubernetes.io/dockerconfigjson'
 fi
