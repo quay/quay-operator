86d0f3a6174ba59d7b20eb85a314d71fe9e18e22
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-15 11:08:17 -0400

    add registry54

--- .travis/setup-sdk.sh
+++ .travis/setup-sdk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 export DEST="${GOPATH}/bin/operator-sdk"
 export SDK_VERSION=v0.10.0
 
 mkdir -p ${GOPATH}/bin
 echo "Downloading the operator-sdk ${SDK_VERSION} into ${DEST}"
-sudo wget -qO- https://github.com/operator-framework/operator-sdk/releases/download/${SDK_VERSION}/operator-sdk-${SDK_VERSION}-x86_64-linux-gnu ${DEST}
+curl https://github.com/operator-framework/operator-sdk/releases/download/${SDK_VERSION}/operator-sdk-${SDK_VERSION}-x86_64-linux-gnu -sLo ${DEST}
 chmod +x ${DEST}
+ls -al
