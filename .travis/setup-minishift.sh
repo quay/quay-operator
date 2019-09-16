e49c6dd16fde9a29c62f2fb3151b48ab078bf8de
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-16 09:56:26 -0400

    lets try okd instead of openshift 3

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -11,7 +11,7 @@ elif [[ -z "${RH_PASSWORD}" ]]; then
 elif [[ -z "${RH_USERNAME}" ]]; then
     echo "RH_USERNAME environment variable not set"
 else
-    sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | sudo tar -xvz -C /bin
+    sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | sudo tar /openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit -xvz -C /bin
     sudo ls /bin
     echo "Logging into quay.io"
     docker login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
