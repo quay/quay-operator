3fd10e5225d5dc36d5e4511d32860a8930b993be
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-16 10:16:20 -0400

    lets try okd instead of openshift 5

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -11,19 +11,19 @@ elif [[ -z "${RH_PASSWORD}" ]]; then
 elif [[ -z "${RH_USERNAME}" ]]; then
     echo "RH_USERNAME environment variable not set"
 else
+    echo "Download oc client"
     sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | sudo tar -xvz -C .
     sudo mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/* /bin
-    sudo ls /bin
     echo "Logging into quay.io"
     docker login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
     cp ~/.docker/config.json ./
-
-    echo "Bring up openshift cluster"
+    echo "Bring up okd cluster"
     IP_ADDR=$(ip addr show $DEV | awk '/inet /{ gsub("/.*", ""); print $2}')
     oc cluster up --public-hostname=${IP_ADDR} --routing-suffix=${IP_ADDR}.nip.io --base-dir=/home/travis/ocp --skip-registry-check=true
+    echo "Login"
     oc login -u system:admin
     echo "Creating new project $QUAY_NAMESPACE"
     oc new-project $QUAY_NAMESPACE
     oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=config.json" --type='kubernetes.io/dockerconfigjson'
-    oc apply -f ./deploy/crd/redhatcop_v1alpha1_quayecosystem_crd.yaml
+    oc apply -f ./deploy/crds/redhatcop_v1alpha1_quayecosystem_crd.yaml
 fi
