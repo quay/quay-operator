5e5d14962976518020593aa257e7861d5e1a8a1a
Christopher Nuland <cnuland@localhost.localdomain>
2019-09-17 17:26:55 -0400

    forgot to add back critical testing components

--- .travis/setup-minishift.sh
+++ .travis/setup-minishift.sh
@@ -9,20 +9,20 @@ elif [[ -z "${QUAY_USERNAME}" ]]; then
 else
     echo "Download oc client"
     sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | sudo tar -xvz -C .
-    sudo mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/* .
+    sudo mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/* /bin
     echo "Logging into quay.io"
     docker login quay.io -u $QUAY_USERNAME -p $QUAY_PASSWORD
     cp ~/.docker/config.json ./
     echo "Bring up okd cluster"
-    ./oc cluster up --skip-registry-check=true
+    oc cluster up --skip-registry-check=true
     echo "Login"
-    ./oc login -u system:admin
+    oc login -u system:admin
     echo "Creating new project $QUAY_NAMESPACE"
-    ./oc new-project $QUAY_NAMESPACE
-    ./oc create serviceaccount quay
-    ./oc adm policy add-scc-to-user anyuid -z quay
-    ./oc adm policy add-cluster-role-to-user cluster-admin admin
-    ./oc login -u admin -p admin
-    ./oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=config.json" --type='kubernetes.io/dockerconfigjson'
-    ./oc apply -f ./deploy/crds/redhatcop_v1alpha1_quayecosystem_crd.yaml
+    oc new-project $QUAY_NAMESPACE
+    oc create serviceaccount quay
+    oc adm policy add-scc-to-user anyuid -z quay
+    oc adm policy add-cluster-role-to-user cluster-admin admin
+    oc login -u admin -p admin
+    oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=config.json" --type='kubernetes.io/dockerconfigjson'
+    oc apply -f ./deploy/crds/redhatcop_v1alpha1_quayecosystem_crd.yaml
 fi
