aab1f26118f55520467c142606fd14b2d089cc0b
Christopher Nuland <cnuland@localhost.localdomain>
2019-12-13 12:12:24 -0500

    added clair test

--- pkg/controller/quayecosystem/quayecosystem_controller_test.go
+++ pkg/controller/quayecosystem/quayecosystem_controller_test.go
@@ -9,11 +9,10 @@ import (
 	"github.com/redhat-cop/operator-utils/pkg/util"
 	redhatcopv1alpha1 "github.com/redhat-cop/quay-operator/pkg/apis/redhatcop/v1alpha1"
 	"github.com/redhat-cop/quay-operator/pkg/controller/quayecosystem/setup"
+	testutil "github.com/redhat-cop/quay-operator/test"
 
 	"github.com/redhat-cop/quay-operator/pkg/controller/quayecosystem/constants"
 	"github.com/stretchr/testify/assert"
-
-	testutil "github.com/redhat-cop/quay-operator/test"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -83,3 +82,72 @@ func TestDefaultConfiguration(t *testing.T) {
 	assert.Equal(t, crd.Spec.Quay.Image, constants.QuayImage)
 
 }
+
+/*
+func TestClairDeployment(t *testing.T) {
+	testutil.SetupLogging()
+	// Stub out object placeholders for test
+	quayEcosystem := &redhatcopv1alpha1.QuayEcosystem{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "QuayEcosystem",
+			APIVersion: "redhatcop.redhat.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+
+	// Add clair enabled, image pull secret, and the name
+	quayEcosystem.Spec.Clair = &redhatcopv1alpha1.Clair{
+		Enabled:             true,
+		ImagePullSecretName: "redhat-pull-secret",
+		Image:               constants.ClairImage,
+	}
+
+	// Objects to track in the fake client.
+	objs := []runtime.Object{
+		quayEcosystem,
+	}
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	s.AddKnownTypes(redhatcopv1alpha1.SchemeGroupVersion, quayEcosystem)
+	if err := ossecurityv1.AddToScheme(s); err != nil {
+		t.Fatal(err, "")
+	}
+	if err := routev1.AddToScheme(s); err != nil {
+		t.Fatal(err, "")
+	}
+	// Initialize fake client
+	cl := fake.NewFakeClient(objs...)
+
+	reconcilerBase := util.NewReconcilerBase(cl, s, nil, nil)
+	r := &ReconcileQuayEcosystem{reconcilerBase: reconcilerBase, k8sclient: nil, quaySetupManager: setup.NewQuaySetupManager(reconcilerBase, nil)}
+
+	// Create the Quay Service Account and Secret
+	err := cl.Create(context.TODO(), testutil.ServiceAccount)
+	assert.NoError(t, err)
+	err = cl.Create(context.TODO(), testutil.SCCAnyUID)
+	assert.NoError(t, err)
+	err = cl.Create(context.TODO(), testutil.Secret)
+	assert.NoError(t, err)
+
+	// Initialize the reconicer request
+	nsn := types.NamespacedName{
+		Name:      name,
+		Namespace: namespace,
+	}
+
+	req := reconcile.Request{
+		NamespacedName: nsn,
+	}
+
+	r.Reconcile(req)
+
+	// Check if the CRD has been created
+	dep := &appsv1.Deployment{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: "quay-operator-clair", Namespace: namespace}, dep)
+	assert.NoError(t, err)
+	assert.Equal(t, dep.ObjectMeta.Name, "quay-operator-clair")
+}
+*/
