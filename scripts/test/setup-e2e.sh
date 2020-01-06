2fd436a88f883a74d9655eee3165c97c94b4a672
Christopher Nuland <cnuland@localhost.localdomain>
2020-01-05 21:54:12 -0500

    E2E setup script now pulls down minishift

--- scripts/test/setup-e2e.sh
+++ scripts/test/setup-e2e.sh
@@ -1,9 +1,9 @@
 export QUAY_NAMESPACE="quay-enterprise"
-#echo "Download oc client"
-#sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | sudo tar -xvz -C .
-#sudo mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/* /bin
-#echo "Bring up okd cluster"
-#oc cluster up --skip-registry-check=true
+echo "Download oc client"
+sudo wget -qO- https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | sudo tar -xvz -C .
+sudo mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/* /bin
+echo "Bring up okd cluster"
+oc cluster up --skip-registry-check=true
 echo "Login"
 oc login -u system:admin
 echo "Creating new project $QUAY_NAMESPACE"
