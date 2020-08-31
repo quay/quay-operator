cc8d720910392280dff69a8ee1fe65f372690eac
Jonathan King <jonathankingfc@gmail.com>
2020-08-31 17:53:37 -0400

    added function to build secret + endpoint

--- main.go
+++ main.go
@@ -18,16 +18,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"net/http"
 	"os"
 
+	jsoniter "github.com/json-iterator/go"
+	quayredhatcomv1 "github.com/quay/quay-operator/api/v1"
+	"github.com/quay/quay-operator/controllers"
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	quayredhatcomv1 "github.com/quay/quay-operator/api/v1"
-	"github.com/quay/quay-operator/controllers"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -80,9 +88,73 @@ func main() {
 	}
 	// +kubebuilder:scaffold:builder
 
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
+	http.HandleFunc("/reconfigure", reconfigureHandler(mgr))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", 7071), nil))
+
+	// setupLog.Info("starting manager")
+	// if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	// 	setupLog.Error(err, "problem running manager")
+	// 	os.Exit(1)
+	// }
+}
+
+// handler to listen for reconfiguration bundle from config-tool
+func reconfigureHandler(mgr manager.Manager) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			w.WriteHeader(404)
+			return
+		}
+
+		var preSecret map[string]interface{}
+		err := yaml.NewDecoder(r.Body).Decode(&preSecret)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		newSecret := createUpdatedSecret(preSecret, "NAME", "NAMESPACE") // NEED TO GET NAMESPACE AND NAME SOMEHOW
+		fmt.Println(newSecret.Data)
+		response := map[string]string{"status": "success"} // TEMP RESPONSE
+		var json = jsoniter.ConfigCompatibleWithStandardLibrary
+		js, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(js)
 	}
+
+}
+
+func createUpdatedSecret(preSecret map[string]interface{}, name, namespace string) corev1.Secret {
+
+	secretData := make(map[string][]byte)
+	for file, contents := range preSecret {
+		if file == "config.yaml" {
+			secretData[file] = encode(contents)
+		} else {
+			secretData[file] = contents.([]byte)
+		}
+
+	}
+
+	newSecret := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: secretData,
+	}
+
+	return newSecret
+}
+
+func encode(value interface{}) []byte {
+	yamlified, _ := yaml.Marshal(value)
+
+	return yamlified
 }
