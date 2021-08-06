2c4f4ece6f2d855e1aaa9379dfe55f22fe8c57e8
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2021-08-06 12:08:41 +0200

    Making reconfigure address customizable
    Allows uses to set the reconfigure address through a command line flag.

--- main.go
+++ main.go
@@ -1,6 +1,4 @@
 /*
-
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -17,12 +15,12 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"sync"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -44,88 +42,144 @@ var (
 	setupLog = ctrl.Log.WithName("setup")
 )
 
-const (
-	operatorPort = 7071
-)
-
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
-
 	_ = quay.AddToScheme(scheme)
 	_ = redhatcop.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
-func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var namespace string
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
+// Arguments holds all accepted command line arguments and flags.
+type Arguments struct {
+	MetricsAddress     string
+	ReconfigureAddress string
+	Namespace          string
+	LeaderElection     bool
+}
+
+// Parse parses command line arguments and flags.
+func (a *Arguments) Parse() {
+	flag.StringVar(
+		&a.MetricsAddress,
+		"metrics-addr",
+		":8080",
+		"The address the metric endpoint binds to.",
+	)
+	flag.StringVar(
+		&a.ReconfigureAddress,
+		"reconfigure-addr",
+		":7071",
+		"The address the reconfigure endpoint binds to.",
+	)
+	flag.BoolVar(
+		&a.LeaderElection,
+		"enable-leader-election",
+		false,
 		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.StringVar(&namespace, "namespace", "", "The Kubernetes namespace that the controller will watch.")
+			"Enabling this will ensure there is only one active controller manager.",
+	)
+	flag.StringVar(
+		&a.Namespace,
+		"namespace",
+		"",
+		"The Kubernetes namespace that the controller will watch.",
+	)
 	flag.Parse()
+}
+
+func main() {
+	var args Arguments
+	args.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: metricsAddr,
-		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "7daa4ab6.quay.redhat.com",
-		Namespace:          namespace,
-		ClientDisableCacheFor: []client.Object{
-			&quay.QuayRegistry{},
+	mgr, err := ctrl.NewManager(
+		ctrl.GetConfigOrDie(),
+		ctrl.Options{
+			Scheme:             scheme,
+			MetricsBindAddress: args.MetricsAddress,
+			Port:               9443,
+			LeaderElection:     args.LeaderElection,
+			LeaderElectionID:   "7daa4ab6.quay.redhat.com",
+			Namespace:          args.Namespace,
 		},
-	})
+	)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
 	var mtx sync.Mutex
-	if err = (&quaycontroller.QuayRegistryReconciler{
+	qyrec := &quaycontroller.QuayRegistryReconciler{
 		Client:         mgr.GetClient(),
 		Log:            ctrl.Log.WithName("controllers").WithName("QuayRegistry"),
 		Scheme:         mgr.GetScheme(),
 		EventRecorder:  mgr.GetEventRecorderFor("quayregistry-controller"),
-		WatchNamespace: namespace,
+		WatchNamespace: args.Namespace,
 		Mtx:            &mtx,
-	}).SetupWithManager(mgr); err != nil {
+	}
+	if err = qyrec.SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "QuayRegistry")
 		os.Exit(1)
 	}
 
-	if err = (&quaycontroller.QuayRegistryStatusReconciler{
+	qstrec := &quaycontroller.QuayRegistryStatusReconciler{
 		Client: mgr.GetClient(),
 		Log:    ctrl.Log.WithName("controllers").WithName("QuayRegistryStatus"),
 		Mtx:    &mtx,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "QuayRegistryStatus")
+	}
+	if err = qstrec.SetupWithManager(mgr); err != nil {
+		setupLog.Error(
+			err, "unable to create controller", "controller", "QuayRegistryStatus",
+		)
 		os.Exit(1)
 	}
 
-	if err = (&redhatcopcontroller.QuayEcosystemReconciler{
+	rhrec := &redhatcopcontroller.QuayEcosystemReconciler{
 		Client: mgr.GetClient(),
 		Log:    ctrl.Log.WithName("controllers").WithName("QuayEcosystem"),
 		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
+	}
+	if err = rhrec.SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "QuayEcosystem")
 		os.Exit(1)
 	}
+
 	// +kubebuilder:scaffold:builder
 
-	setupLog.Info("starting server on port 7071")
-	go func() {
-		http.HandleFunc("/reconfigure", configure.ReconfigureHandler(mgr.GetClient()))
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", operatorPort), nil))
-	}()
+	ctx := ctrl.SetupSignalHandler()
+	go setupReconfigureEndpoint(ctx, args.ReconfigureAddress, mgr.GetClient())
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+// setupReconfigureEndpoint sets up an http server to deal with reconfigure requests made by the
+// quay config tool. This function only returns when provided context has been "cancelled".
+func setupReconfigureEndpoint(ctx context.Context, addr string, cli client.Client) {
+	setupLog = ctrl.Log.WithName("setup").WithName("reconfigure")
+	setupLog.Info("starting reconfigure endpoint server", "address", addr)
+
+	hfunc := configure.ReconfigureHandler(cli)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: http.HandlerFunc(hfunc),
+	}
+
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			setupLog.Error(err, "error shutting down reconfigure endpoint server")
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		setupLog.Error(err, "error shutting down reconfigure endpoint server")
+		return
+	}
+	setupLog.Info("reconfigure server has been shutdown")
 }
