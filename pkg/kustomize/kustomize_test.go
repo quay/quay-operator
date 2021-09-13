1de2158e5c089330493c1b0cb1a85dadb3a7851b
Jonathan <jonathankingfc@gmail.com>
2021-09-13 13:33:09 -0400

    rbac: Backport rbac upgrade to 3.5 branch (PROJQUAY-2531)
    - Update rbac kind from v1beta1 to v1

--- pkg/kustomize/kustomize_test.go
+++ pkg/kustomize/kustomize_test.go
@@ -11,7 +11,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbac "k8s.io/api/rbac/v1beta1"
+	rbac "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
