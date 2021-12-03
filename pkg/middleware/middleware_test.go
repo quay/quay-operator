a1a1550f8b84555fc00a0f2a2938f2ebd9efbb91
Jonathan King <jonathankingfc@gmail.com>
2021-12-03 13:50:42 -0500

    overrides: Allow for volume size overrides (PROJQUAY-1090) (#586)
    - Add override field to Operator CRD to allow for volume size overrides
    - Add tests to reflect changes in API

--- pkg/middleware/middleware_test.go
+++ pkg/middleware/middleware_test.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"fmt"
 	"testing"
 
 	route "github.com/openshift/api/route/v1"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -138,20 +140,133 @@ var processTests = []struct {
 		},
 		nil,
 	},
+	{
+		"volumeSizeDefault",
+		&quaycontext.QuayRegistryContext{},
+		&v1.QuayRegistry{
+			Spec: v1.QuayRegistrySpec{
+				Components: []v1.Component{
+					{Kind: "route", Managed: true},
+					{Kind: "tls", Managed: true},
+				},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "postgres"},
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceStorage: resource.MustParse("50Gi")}},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "postgres"},
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceStorage: resource.MustParse("50Gi")}},
+			},
+		},
+		nil,
+	},
+	{
+		"volumeSizeOverridePostgres",
+		&quaycontext.QuayRegistryContext{},
+		&v1.QuayRegistry{
+			Spec: v1.QuayRegistrySpec{
+				Components: []v1.Component{
+					{Kind: "route", Managed: true},
+					{Kind: "tls", Managed: true},
+					{Kind: "postgres", Managed: true, Overrides: &v1.Override{VolumeSize: parseResourceString("60Gi")}},
+				},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "postgres"},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "postgres"},
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceStorage: resource.MustParse("60Gi")}},
+			},
+		},
+		nil,
+	},
+	{
+		"volumeSizeOverrideClairPostgres",
+		&quaycontext.QuayRegistryContext{},
+		&v1.QuayRegistry{
+			Spec: v1.QuayRegistrySpec{
+				Components: []v1.Component{
+					{Kind: "route", Managed: true},
+					{Kind: "tls", Managed: true},
+					{Kind: "postgres", Managed: true, Overrides: &v1.Override{VolumeSize: parseResourceString("70Gi")}},
+					{Kind: "clair", Managed: true, Overrides: &v1.Override{VolumeSize: parseResourceString("60Gi")}},
+				},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "clair-postgres"},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "clair-postgres"},
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceStorage: resource.MustParse("60Gi")}},
+			},
+		},
+		nil,
+	},
+	{
+		"volumeSizeShrinkError",
+		&quaycontext.QuayRegistryContext{},
+		&v1.QuayRegistry{
+			Spec: v1.QuayRegistrySpec{
+				Components: []v1.Component{
+					{Kind: "route", Managed: true},
+					{Kind: "tls", Managed: true},
+					{Kind: "postgres", Managed: true, Overrides: &v1.Override{VolumeSize: parseResourceString("30Gi")}},
+				},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{"quay-component": "postgres"},
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				Resources: corev1.ResourceRequirements{Requests: corev1.ResourceList{corev1.ResourceStorage: resource.MustParse("50Gi")}},
+			},
+		},
+		nil,
+		fmt.Errorf("cannot shrink volume override size from 50Gi to 30Gi"),
+	},
 }
 
 func TestProcess(t *testing.T) {
 	assert := assert.New(t)
-
 	for _, test := range processTests {
-		processedObj, err := Process(test.ctx, test.quay, test.obj)
 
-		if test.expectedError != nil {
-			assert.Error(err, test.name)
-		} else {
-			assert.Nil(err, test.name)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			processedObj, err := Process(test.ctx, test.quay, test.obj)
+			if test.expectedError != nil {
+				assert.Error(err, test.name)
+			} else {
+				assert.Nil(err, test.name)
+			}
+			assert.Equal(test.expected, processedObj, test.name)
+		})
 
-		assert.Equal(test.expected, processedObj, test.name)
 	}
+}
+
+func parseResourceString(s string) *resource.Quantity {
+	resourceSize := resource.MustParse(s)
+	return &resourceSize
 }
