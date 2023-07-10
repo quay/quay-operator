f24b505d39daeec71b333e5c563c4e597b50986b
Oleg Bulatov <oleg@bulatov.me>
2023-07-10 17:25:53 +0200

    chore: Update prepare-upstream.sh
    And reformat quay-operator.clusterserviceversion.yaml via yq

--- hack/prepare-upstream.sh
+++ hack/prepare-upstream.sh
@@ -1,11 +1,27 @@
-sed -i "s/createdAt:.*/createdAt: `date -u +'%Y-%m-%d %k:%m UTC'`/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/olm\.skipRange:.*/olm\.skipRange: \">=3.6.x <${RELEASE}\"/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/quay-version:.*/quay-version: ${RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/containerImage:.*/containerImage: quay.io\/projectquay\/quay-operator:v${RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/^  name: quay-operator.*/  name: quay-operator.v${RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/image: quay.io\/projectquay\/quay-operator.*/image: quay.io\/projectquay\/quay-operator:v${RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/value: quay.io\/projectquay\/quay:.*/value: quay.io\/projectquay\/quay:${QUAY_RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/value: quay.io\/projectquay\/clair:.*/value: quay.io\/projectquay\/clair:${CLAIR_RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/^  version: .*/  version: ${RELEASE}/" bundle/manifests/quay-operator.clusterserviceversion.yaml
-sed -i "s/operators.operatorframework.io.bundle.channel.default.v1.*/operators.operatorframework.io.bundle.channel.default.v1: ${CHANNEL}/" bundle/metadata/annotations.yaml
-sed -i "s/operators.operatorframework.io.bundle.channels.v1.*/operators.operatorframework.io.bundle.channels.v1: ${CHANNEL}/" bundle/metadata/annotations.yaml
+#!/bin/sh -e
+
+# Single quotes for expressions are used intentionally to delegate variable
+# substitution to yq.
+# shellcheck disable=SC2016
+
+yq eval -i '
+    .metadata.annotations.createdAt = (now | tz("UTC") | format_datetime("2006-01-02 15:04 UTC")) |
+    .metadata.annotations["olm.skipRange"] = (">=3.6.x <${RELEASE}" | envsubst) |
+    .metadata.annotations["quay-version"] = strenv(RELEASE) |
+    .metadata.annotations.containerImage = ("quay.io/projectquay/quay-operator:v${RELEASE}" | envsubst) |
+    .metadata.name = ("quay-operator.v${RELEASE}" | envsubst) |
+    .spec.install.spec.deployments[0].spec.template.spec.containers[0].image = ("quay.io/projectquay/quay-operator:v${RELEASE}" | envsubst) |
+    .spec.install.spec.deployments[0].spec.template.spec.containers[0].env[] |= (
+        select(.name == "RELATED_IMAGE_COMPONENT_BUILDER").value = ("quay.io/projectquay/quay-builder:v${RELEASE}" | envsubst) |
+        select(.name == "RELATED_IMAGE_COMPONENT_CLAIR").value = ("quay.io/projectquay/clair:v${CLAIR_RELEASE}" | envsubst) |
+        select(.name == "RELATED_IMAGE_COMPONENT_QUAY").value = ("quay.io/projectquay/quay:v${RELEASE}" | envsubst)
+    ) |
+    .spec.version = strenv(RELEASE) |
+    .spec.replaces = strenv(REPLACES)
+' bundle/manifests/quay-operator.clusterserviceversion.yaml
+
+yq eval -i '
+    .annotations["operators.operatorframework.io.bundle.channel.default.v1"] = strenv(DEFAULT_CHANNEL) |
+    .annotations["operators.operatorframework.io.bundle.channels.v1"] = strenv(CHANNEL) |
+    .annotations["operators.operatorframework.io.bundle.package.v1"] = "project-quay"
+' bundle/metadata/annotations.yaml
