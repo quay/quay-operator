6dc0e893a07ad2500bfe047e2c2608b004857ef5
Oleg Bulatov <oleg@bulatov.me>
2023-07-11 15:55:11 +0200

    chore: Upstream tags don't have prefix v

--- hack/prepare-upstream.sh
+++ hack/prepare-upstream.sh
@@ -8,13 +8,13 @@ yq eval -i '
     .metadata.annotations.createdAt = (now | tz("UTC") | format_datetime("2006-01-02 15:04 UTC")) |
     .metadata.annotations["olm.skipRange"] = (">=3.6.x <${RELEASE}" | envsubst) |
     .metadata.annotations["quay-version"] = strenv(RELEASE) |
-    .metadata.annotations.containerImage = ("quay.io/projectquay/quay-operator:v${RELEASE}" | envsubst) |
+    .metadata.annotations.containerImage = ("quay.io/projectquay/quay-operator:${RELEASE}" | envsubst) |
     .metadata.name = ("quay-operator.v${RELEASE}" | envsubst) |
-    .spec.install.spec.deployments[0].spec.template.spec.containers[0].image = ("quay.io/projectquay/quay-operator:v${RELEASE}" | envsubst) |
+    .spec.install.spec.deployments[0].spec.template.spec.containers[0].image = ("quay.io/projectquay/quay-operator:${RELEASE}" | envsubst) |
     .spec.install.spec.deployments[0].spec.template.spec.containers[0].env[] |= (
-        select(.name == "RELATED_IMAGE_COMPONENT_BUILDER").value = ("quay.io/projectquay/quay-builder:v${RELEASE}" | envsubst) |
-        select(.name == "RELATED_IMAGE_COMPONENT_CLAIR").value = ("quay.io/projectquay/clair:v${CLAIR_RELEASE}" | envsubst) |
-        select(.name == "RELATED_IMAGE_COMPONENT_QUAY").value = ("quay.io/projectquay/quay:v${RELEASE}" | envsubst)
+        select(.name == "RELATED_IMAGE_COMPONENT_BUILDER").value = ("quay.io/projectquay/quay-builder:${RELEASE}" | envsubst) |
+        select(.name == "RELATED_IMAGE_COMPONENT_CLAIR").value = ("quay.io/projectquay/clair:${CLAIR_RELEASE}" | envsubst) |
+        select(.name == "RELATED_IMAGE_COMPONENT_QUAY").value = ("quay.io/projectquay/quay:${RELEASE}" | envsubst)
     ) |
     .spec.version = strenv(RELEASE) |
     .spec.replaces = strenv(REPLACES)
